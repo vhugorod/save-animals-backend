83e29fba281bee69dd46fcbe835fc389cfd24864
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-04-21 22:58:44 -0300

    chore: Routes separated from server

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
+app.use(express.json());
 
-  response.json([
-    'Vitor',
-    'Carlos',
-    'Pedro',
-  ])
-});
+app.use(routes);
 
 app.listen(3333);
