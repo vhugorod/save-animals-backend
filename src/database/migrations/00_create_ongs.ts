e41a5bc32f92d452f44a525489e124ea645c98f2
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-04-22 00:22:20 -0300

    feat: Animals and Ong_animals tables Migration

--- src/database/migrations/00_create_ongs.ts
+++ src/database/migrations/00_create_ongs.ts
@@ -3,13 +3,15 @@ import { Knex } from 'knex';
 export async function up(knex: Knex) {
   return knex.schema.createTable('ongs', table => {
     table.increments('id').primary();
-    table.string('iamge').notNullable();
+    table.string('image').notNullable();
     table.string('name').notNullable();
+    table.string('expedient').notNullable();
     table.string('cellphone').notNullable();
     table.decimal('latitude').notNullable();
     table.decimal('longitude').notNullable();
     table.string('city').notNullable();
     table.string('uf', 2).notNullable();
+    table.string('recommendations').notNullable();
   })
 } 
 
--- src/database/migrations/02_create_ong_animals.ts
+++ src/database/migrations/02_create_ong_animals.ts
@@ -1,7 +1,21 @@
-export async function up() {
+import { Knex } from 'knex';
 
+export async function up(knex: Knex) {
+  return knex.schema.createTable('ong_animals', table => {
+    table.increments('id').primary();
+
+    table.integer('ong_id')
+      .notNullable()
+      .references('id')
+      .inTable('ongs');
+
+    table.integer('animal_id')
+      .notNullable()
+      .references('id')
+      .inTable('animals');
+  })
 } 
 
-export async function down() {
-  
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('ong_animals');
 } 
